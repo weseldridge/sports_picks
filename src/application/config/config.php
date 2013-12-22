dff74a94fc044f4c6015d797ae26d6038c67d19b
Wes Eldridge <@>
2013-12-22 12:22:07 -0600

    Config stuff

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://localhost/personal/the_league/src/';
+//$config['base_url']	= 'http://localhost/personal/the_league/src/';
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +28,14 @@ $config['base_url']	= '';
 |
 */
 $config['index_page'] = 'index.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Site Full URL
+|--------------------------------------------------------------------------
+*/
+$config['full_url'] = $config['base_url'] . $config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -293,7 +302,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
