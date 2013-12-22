134f23d3359d0097ffb67affcb5308baefe44261
Wes Eldridge <@>
2013-12-22 13:47:44 -0600

    User controller load view and process login

--- src/application/controllers/users.php
+++ src/application/controllers/users.php
@@ -7,4 +7,53 @@ class Users extends CI_Controller {
         parent::__construct();
         session_start();
     }
+
+    public function login()
+    {
+    	if(isset($_SESSION['username']))
+        {
+            redirect('league');
+        }
+    	$this->load->view('template/header');
+    	$this->load->view('user/login');
+    	$this->load->view('template/footer');
+    }
+
+    public function user_login()
+    {
+    	if($_SERVER['RESQUEST_METHOD'] == 'POST') {
+	    		
+	        $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');
+	        $this->form_validation->set_rules('username', 'Username', 'required');
+
+	        if($this->form_validation->run() !== false ) {
+
+	        	$this->load->model('Users_model');
+	        	// Check to see if user is valid
+	        	// Returns false or a user
+	        	$user = $this->Users_model->verify_user($this->input->post('username'),
+	        											$this->input->post('password'));
+
+	        	// If valid user set session information.
+	        	if($user){
+        			$_SESSION['username'] = $user['username'];
+        			$_SESSION['user_id'] = $user['id'];
+        			$_SESSION['user_level'] = $user['user_level'];
+
+        			redirect('league');
+	        	} else {
+	        		$this->load->view('template/header');
+	    			$this->load->view('user/login');
+	    			$this->load->view('template/footer');
+	        	}
+	        } else {
+	        	$this->load->view('template/header');
+    			$this->load->view('user/login');
+    			$this->load->view('template/footer');
+	        }
+
+    	} else {
+    		redirect('user/login');
+    	}
+    }
 }
